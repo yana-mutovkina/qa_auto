ca54a5651b075037d3706691e3aa9236de414917
Yana Mutovkina <yana.mutovkina@gmail.com>
2023-10-24 20:37:12 +0300

    API tests

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from modules.api.clients.gihub import GitHub
 
 
 class User:
@@ -8,12 +9,12 @@ class User:
         self.second_name = None
 
     def create(self):
-        self.name = "Sergii"
-        self.second_name = "Butenko"
+        self.name = 'Sergii'
+        self.second_name = 'Butenko'
 
     def remove(self):
-        self.name = ""
-        self.second_name = ""
+        self.name = ''
+        self.second_name = ''
 
 
 @pytest.fixture
@@ -24,3 +25,9 @@ def user():
     yield user
 
     user.remove()
+
+
+@pytest.fixture
+def github_api():
+    api = GitHub()
+    yield api
--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -1,8 +1,10 @@
 import pytest
 
+
 @pytest.mark.check
 def test_change_name(user):
     assert user.name == 'Sergii'
+
 
 @pytest.mark.check
 def test_change_second_name(user):
